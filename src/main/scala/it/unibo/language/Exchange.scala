2de8c9fb1809f2038c0e9c82d540d1e98f413fdc
Gianluca Aguzzi <gianluca.aguzzi@studio.unibo.it>
2023-12-15 10:10:26 +0100

    chore: improve code style

--- src/main/scala/it/unibo/language/Exchange.scala
+++ src/main/scala/it/unibo/language/Exchange.scala
@@ -1,24 +1,25 @@
 package it.unibo.language
 import it.unibo.core.{VM, Tag, Empty, Local}
-object Exchange {
+object Exchange:
+  private val exchangeTag = Tag("exchange")
   def exchange[A: Reader: Writer](
       init: => A
-  )(logic: (Map[Int, A]) => (Map[Int, A], Map[Int, A]))(using vm: VM): Map[Int, A] = {
-    vm.enter(Tag("exchange"))
+  )(logic: (Map[Int, A]) => (Map[Int, A], Map[Int, A]))(using vm: VM): Map[Int, A] =
+    vm.enter(exchangeTag)
+    // eval only once
     val default = init
-    val aligned = Map(
-      vm.context.self -> default
-    ) ++ vm.received
+    // unmarshalling received data
+    val receivedUnmarshalled = vm.received
       .map { case (id, data) => (id, summon[Reader[A]](data)) }
-    val neighValue = aligned
-    val (ret, toSend) = logic(neighValue)
-    val notification: Map[Int, Local] = vm.context.neighbours.map { id =>
-      id -> summon[Writer[A]](default)
-    }.toMap ++ toSend.map { case (id, data) => id -> summon[Writer[A]](data) }
-    vm.send(notification)
+    val alignedNeighborhoodData = Map(vm.context.self -> default) ++ receivedUnmarshalled
+    val (ret, toSend) = logic(alignedNeighborhoodData)
+    val toSendMarshalled = toSend.map { case (id, data) => (id, summon[Writer[A]](data)) }
+    val notifications = vm.context.neighbours
+      .map(id => id -> summon[Writer[A]](default)) // send default value to all neighbours, to align the neighbourhood
+      .toMap ++ toSendMarshalled
+    vm.send(notifications)
     vm.exit()
     ret
-  }
 
   def exchangeRetSend[A: Reader: Writer](init: => A)(logic: Map[Int, A] => Map[Int, A])(using vm: VM): Map[Int, A] = {
     exchange(init) { neighValue =>
@@ -27,6 +28,7 @@ object Exchange {
     }
   }
 
+  // just an example of how to use exchange to implement a rep, please use the FP version
   def rep[A: Reader: Writer](init: => A)(loop: A => A)(using vm: VM): A =
     import FieldCalculus.mid
     val result = exchangeRetSend[A](init) { neighValue =>
@@ -37,4 +39,3 @@ object Exchange {
   def nbr[A: Reader: Writer](data: => A)(using vm: VM): Map[Int, A] = exchange(data) { neighValue =>
     (neighValue, neighValue.map(_._1 -> data))
   }
-}
